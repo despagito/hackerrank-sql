b4bfd7b43686c09bca4c6b79200a8fe20e4dbe2f
Despagito <9820592+despagito@users.noreply.github.com>
2020-03-16 20:15:07 -0400

    Update weather-observation-station-8.sql

--- basic-select/weather-observation-station-8.sql
+++ basic-select/weather-observation-station-8.sql
@@ -6,3 +6,8 @@ SELECT DISTINCT City
 FROM Station
 WHERE LEFT(City, 1) IN ('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U') AND
       RIGHT(City, 1) IN ('a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U');
+
+-- Using REGEXP
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[aeiou].*[aeiou]$'

77578e189b9c41d1177166f88c7549160943b817
Despagito <9820592+despagito@users.noreply.github.com>
2020-03-16 20:17:58 -0400

    Update weather-observation-station-10.sql

--- basic-select/weather-observation-station-10.sql
+++ basic-select/weather-observation-station-10.sql
@@ -5,3 +5,9 @@
 SELECT DISTINCT City
 FROM Station
 WHERE RIGHT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+
+
+-- Using REGEXP
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '.*[^aeiou]$';
